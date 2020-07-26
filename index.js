0f2d15b9a781b2be0681aedb20210cec80a40466
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2020-07-26 23:12:11 +0300

    defenitely drunk

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.get('/data', async (_req, res) => {
 
 app.get('/appinfo', async (req, res) => {
   const { countryCode } = req.headers;
+  console.log(countryCode);
   const countries = await CountryList.findOne({ id: 1 });
   if (!countries) {
     return res.json({
