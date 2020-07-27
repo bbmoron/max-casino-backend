3eec7090ff23d09fb32806325e9adc0ff1745e33
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2020-07-27 16:19:11 +0300

    wow, finally coding sober... NICE

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
+const cookieparser = require('cookie-parser');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-app.use(express.static(process.env.STATIC));
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cookieparser());
 
 const countriesModel = new mongoose.Schema({
   code: String,
@@ -29,7 +31,26 @@ const linksModel = new mongoose.Schema({
 });
 const Link = mongoose.model('Link', linksModel);
 
-app.get('/', (_req, res) => res.sendFile('index.html'));
+app.get('/', (req, res) => {
+  if (!req.cookies) return res.redirect('/auth');
+  return res.sendFile(path.join(`${__dirname}/public/index.html`));
+});
+
+app.get('/success', (_req, res) => res.redirect('/'));
+
+app.get('/auth', (_req, res) => res.sendFile(path.join(`${__dirname}/public/auth.html`)));
+
+app.post('/auth', (req, res) => {
+  if (process.env.PASSWORD !== req.body.password) {
+    return res.json({
+      allowed: false,
+    });
+  }
+  res.cookie('authorized', 1, { maxAge: 90000 });
+  return res.json({
+    allowed: true,
+  });
+});
 
 app.get('/data', async (_req, res) => {
   const countries = await CountryList.findOne({ id: 1 });
